94a959e74fedca0b43cd340b4bce8da493e91462
Bilal094 <99066318@mydavinci.nl>
2023-05-17 11:04:12 +0200

    Assignment 1 done

---
+++ Pokemon-Battle-Simulator/Pokemon-Battle-Simulator/Program.cs
@@ -0,0 +1,47 @@
+﻿/* 1.The player start the game.
+2. The player gives a name to a charmander.
+3. The charmander does its battle cry for ten times.
+4. The player can give a new name to the same charmander.
+5. The charmander does its battle cry for ten times.
+6. Repeat 4 and 5 until the player quits the game. */
+
+
+// Main program
+class Program
+{
+    static void Main(string[] args)
+    {
+        Charmander pokemon = new Charmander("Charmander");
+        
+        while (true)
+        {
+            Console.WriteLine("What will you name the charmander? Type 'n' to end the application");
+            string newName = Console.ReadLine().ToLower();
+
+            if (newName != "n")
+            {
+                pokemon.name = newName;
+            }
+            else
+            {
+                break;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(pokemon.name + " does its battle cry 10 times");
+            }
+        }
+    }
+}
+
+// Pokemon Classes
+class Charmander
+{
+    public string name;
+    public string strength = "fire";
+    public Charmander(string defaultName)
+    {
+        name = defaultName;
+    }
+}
